357686955bbf87c1bdbaaae3981d5a3d6e1d47ac
Wilton-Lucas <willstd00@gmail.com>
2021-04-01 16:57:05 -0300

    controller

--- angularApp/src/controllers/mainController.js
+++ angularApp/src/controllers/mainController.js
@@ -1,8 +1,50 @@
+class Calculadora {
+
+    constructor() {
+        this.total = 0;
+    }
+
+    somar(numero) {
+        this.total += numero;
+    }
+
+    subtrair(numero) {
+        this.total += numero;
+    }
+
+    multiplicar(numero) {
+        this.total += numero;
+    }
+
+    dividir(numero) {
+        if (numero > 0)
+            this.total += numero;
+    }
+
+    limpar() {
+        this.total = 0;
+    }
+
+    getTotal() {
+        return this.total;
+    }
+
+}
+
+c = new Calculadora();
 
 angular.module('app', []).controller('controlador', ($scope) => {
-    $scope.mensagem = 'teste';
-})
 
+
+    $scope.valorDigitado = 0;
+    $scope.operacao = 0;
+    $scope.total = c.getTotal();
+    $scope.display = c.getTotal() + $scope.valorDigitado;
+    $scope.somar = c.somar($scope.valorDigitado);
+    $scope.subtrair = c.subtrair($scope.valorDigitado);
+    $scope.multiplicar = c.multiplicar($scope.valorDigitado);
+    $scope.dividir = c.dividir($scope.valorDigitado);
+})
 
 
 
